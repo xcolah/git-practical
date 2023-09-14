0a2c7cd6856143ce874cfda1f7886708665888ed
Jeremiah Ang <jeremiahang94@gmail.com>
2023-09-14 02:38:09 +0000

    Add test for hangman

--- public/hangman.js
+++ public/hangman.js
@@ -1,110 +1,116 @@
 // set up the hangman game
 const words = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew'];
-const maxGuesses = 6;
-let word = words[Math.floor(Math.random() * words.length)];
-let guessedLetters = new Set();
-let guessesRemaining = maxGuesses;
 
-const wordDisplay = document.getElementById('word-display');
-const guessesRemainingText = document.getElementById('guesses-remaining');
-const keyboard = document.getElementById('keyboard');
+function startGame(word = words[Math.floor(Math.random() * words.length)], maxGuesses = 6) {
+    console.log(word);
+    let guessedLetters = new Set();
+    let guessesRemaining = maxGuesses;
 
-// HINT
-const guessResult = document.getElementById('guess-result');
+    const wordDisplay = document.getElementById('word-display');
+    const guessesRemainingText = document.getElementById('guesses-remaining');
+    const keyboard = document.getElementById('keyboard');
 
-// initialize the word display with underscores for each letter in the word
-let displayText = '';
-for (let i = 0; i < word.length; i++) {
-    displayText += '_ ';
-}
-wordDisplay.textContent = displayText.trim();
+    // HINT
+    const guessResult = document.getElementById('guess-result');
 
-// update the guesses remaining display
-guessesRemainingText.textContent = `Guesses remaining: ${guessesRemaining}`;
+    // initialize the word display with underscores for each letter in the word
+    let displayText = '';
+    for (let i = 0; i < word.length; i++) {
+        displayText += '_ ';
+    }
+    wordDisplay.textContent = displayText.trim();
 
-// enable the buttons for the letters that have not been guessed yet
-const enableButtons = () => {
-    const buttons = keyboard.querySelectorAll('.key');
-    buttons.forEach((letterButton) => {
-        if (!guessedLetters.has(letterButton.textContent)) {
-            letterButton.disabled = false;
-        }
-    });
-};
+    // update the guesses remaining display
+    guessesRemainingText.textContent = `Guesses remaining: ${guessesRemaining}`;
 
-const disableButtons = () => {
-    const buttons = keyboard.querySelectorAll('.key');
-    buttons.forEach((letterButton) => {
-        if (!guessedLetters.has(letterButton.textContent)) {
-            letterButton.disabled = true;
-        }
-    });
-};
+    // enable the buttons for the letters that have not been guessed yet
+    const enableButtons = () => {
+        const buttons = keyboard.querySelectorAll('.key');
+        buttons.forEach((letterButton) => {
+            if (!guessedLetters.has(letterButton.textContent)) {
+                letterButton.disabled = false;
+            }
+        });
+    };
 
-const disableButton = (letter) => {
-    const button = keyboard.querySelector(`.key[value="${letter}"]`);
-    button.disabled = true;
-};
+    const disableButtons = () => {
+        const buttons = keyboard.querySelectorAll('.key');
+        buttons.forEach((letterButton) => {
+            if (!guessedLetters.has(letterButton.textContent)) {
+                letterButton.disabled = true;
+            }
+        });
+    };
 
-// update the keyboard display
-const updateKeyboard = () => {
-    const buttons = keyboard.querySelectorAll('.key');
-    buttons.forEach((letterButton) => {
-        if (guessedLetters.has(letterButton.textContent)) {
-            letterButton.classList.add('guessed');
+    const disableButton = (letter) => {
+        const button = keyboard.querySelector(`.key[value="${letter}"]`);
+        button.disabled = true;
+    };
+
+    // update the keyboard display
+    const updateKeyboard = () => {
+        const buttons = keyboard.querySelectorAll('.key');
+        buttons.forEach((letterButton) => {
+            if (guessedLetters.has(letterButton.textContent)) {
+                letterButton.classList.add('guessed');
+            } else {
+                letterButton.classList.remove('guessed');
+            }
+        });
+    };
+
+    const processGuess = (guess) => {
+        if (guessedLetters.has(guess)) {
+            guessResult.textContent = 'You already guessed that letter.';
+        } else if (guess.length !== 1) {
+            guessResult.textContent = 'Please enter a single letter.';
+        } else if (!/[a-z]/.test(guess)) {
+            guessResult.textContent = 'Please enter a letter.';
         } else {
-            letterButton.classList.remove('guessed');
-        }
-    });
-};
+            guessedLetters.add(guess);
 
-const processGuess = (guess) => {
-    if (guessedLetters.has(guess)) {
-        guessResult.textContent = 'You already guessed that letter.';
-    } else if (guess.length !== 1) {
-        guessResult.textContent = 'Please enter a single letter.';
-    } else if (!/[a-z]/.test(guess)) {
-        guessResult.textContent = 'Please enter a letter.';
-    } else {
-        guessedLetters.add(guess);
-
-        if (word.includes(guess)) {
-            // the guess was correct
-            let displayArray = wordDisplay.textContent.split(' ');
-            for (let i = 0; i < word.length; i++) {
-                if (word[i] === guess) {
-                    displayArray[i] = guess;
+            if (word.includes(guess)) {
+                // the guess was correct
+                let displayArray = wordDisplay.textContent.split(' ');
+                for (let i = 0; i < word.length; i++) {
+                    if (word[i] === guess) {
+                        displayArray[i] = guess;
+                    }
+                }
+                wordDisplay.textContent = displayArray.join(' ');
+                if (!wordDisplay.textContent.includes('_')) {
+                    guessResult.textContent = 'Congratulations, you won!';
+                    disableButtons();
+                }
+            } else {
+                // the guess was incorrect
+                guessesRemaining--;
+                guessesRemainingText.textContent = `Guesses remaining: ${guessesRemaining}`;
+                if (guessesRemaining === 0) {
+                    guessResult.textContent = `Sorry, you lost. The word was "${word}".`;
+                    disableButtons();
+                } else {
+                    guessResult.textContent = 'Incorrect guess.';
                 }
             }
-            wordDisplay.textContent = displayArray.join(' ');
-            if (!wordDisplay.textContent.includes('_')) {
-                guessResult.textContent = 'Congratulations, you won!';
-                disableButtons();
-            }
-        } else {
-            // the guess was incorrect
-            guessesRemaining--;
-            guessesRemainingText.textContent = `Guesses remaining: ${guessesRemaining}`;
-            if (guessesRemaining === 0) {
-                guessResult.textContent = `Sorry, you lost. The word was "${word}".`;
-                disableButtons();
-            } else {
-                guessResult.textContent = 'Incorrect guess.';
-            }
+
+            disableButton(guess);
+            updateKeyboard();
         }
+    };
 
-        disableButton(guess);
-        updateKeyboard();
-    }
-};
+    // initialize the keyboard display
+    enableButtons();
 
-// initialize the keyboard display
-enableButtons();
+    // add event listeners to the keyboard buttons
+    keyboard.onclick = (event) => {
+        const target = event.target;
+        if (target.classList.contains('key')) {
+            processGuess(target.textContent);
+        }
+    };
 
-// add event listeners to the keyboard buttons
-keyboard.addEventListener('click', (event) => {
-    const target = event.target;
-    if (target.classList.contains('key')) {
-        processGuess(target.textContent);
-    }
-});
+    return word;
+}
+
+startGame();
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -7,3 +7,51 @@ test('has studentId', async ({ page }) => {
     // Expect a title "to contain" a substring.
     await expect(page.getByTestId('student-id')).toHaveText(/P[0-9]{7}/);
 });
+
+test('hangman game completes successfully', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+    const word = await page.evaluate(() => window.startGame('donkey'));
+
+    for (let i = 0; i < word.length; i++) {
+        const character = word[i];
+        await expect(page.locator('.key').getByText(character)).not.toBeDisabled();
+        await page.locator('.key').getByText(character).click();
+        await expect(page.locator('.key').getByText(character)).toBeDisabled();
+        await expect(page.locator('#guesses-remaining')).toHaveText(`Guesses remaining: 6`);
+    }
+
+    await expect(page.locator('#guess-result')).toHaveText('Congratulations, you won!');
+});
+
+test('hangman game detects wrong guesses and still completes successfully', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+    const guessCount = 6;
+    const word = await page.evaluate(() => window.startGame('donkey', 6));
+
+    async function clickKeyboard(character, guessRemainingText) {
+        await expect(page.locator('.key').getByText(character)).not.toBeDisabled();
+        await page.locator('.key').getByText(character).click();
+        await expect(page.locator('.key').getByText(character)).toBeDisabled();
+        await expect(page.locator('#guesses-remaining')).toHaveText(guessRemainingText);
+    }
+
+    const part1 = word.slice(0, 3);
+    for (let i = 0; i < part1.length; i++) {
+        const character = part1[i];
+        await clickKeyboard(character, `Guesses remaining: ${guessCount}`);
+    }
+
+    const partWrong = 'abc';
+    for (let i = 0; i < partWrong.length; i++) {
+        const character = partWrong[i];
+        await clickKeyboard(character, `Guesses remaining: ${guessCount - i - 1}`);
+    }
+
+    const part2 = word.slice(3);
+    for (let i = 0; i < part2.length; i++) {
+        const character = part2[i];
+        await clickKeyboard(character, `Guesses remaining: ${guessCount - partWrong.length}`);
+    }
+
+    await expect(page.locator('#guess-result')).toHaveText('Congratulations, you won!');
+});
