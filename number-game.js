60f07ff5b4591d425db1052b7645809e2e1936f8
Jeremiah Ang (SP) <jeremiah_ang@sp.edu.sg>
2023-09-14 02:38:53 +0000

    Reports current range

--- number-game.js
+++ number-game.js
@@ -1,11 +1,16 @@
-const minNumber = 0;
-const maxNumber = 100;
+// set up the number guessing game
+let minNumber = 0;
+let maxNumber = 100;
 let randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
 let guessCount = 0;
 
 const guessInput = document.getElementById('guess-input');
 const guessButton = document.getElementById('guess-button');
 const guessResult = document.getElementById('guess-result');
+const rangeText = document.getElementById('range');
+
+// display the latest min and max range
+rangeText.textContent = `Guess a number between ${minNumber} and ${maxNumber}:`;
 
 guessButton.addEventListener('click', () => {
     const guess = parseInt(guessInput.value);
@@ -17,13 +22,18 @@ guessButton.addEventListener('click', () => {
     } else if (guess < randomNumber) {
         guessResult.textContent = 'Too low!';
         guessCount++;
+        minNumber = guess + 1;
     } else if (guess > randomNumber) {
         guessResult.textContent = 'Too high!';
         guessCount++;
+        maxNumber = guess - 1;
     } else {
         guessCount++;
         guessResult.textContent = `Congratulations! You guessed the number ${randomNumber} in ${guessCount} tries.`;
         guessInput.disabled = true;
         guessButton.disabled = true;
     }
+
+    // update the latest min and max range
+    rangeText.textContent = `Guess a number between ${minNumber} and ${maxNumber}:`;
 });
